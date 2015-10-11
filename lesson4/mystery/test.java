a3bab2b9ff388d22456e81684eff4c403dd22874
Jaap de Jong <jaap.dejong@nedap.com>
2015-10-11 21:44:23 +0200

    Reorganize directories

---
+++ lesson4/mystery/test.java
@@ -0,0 +1,30 @@
+
+/**
+ * Write a description of class test here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class test
+{
+    // instance variables - replace the example below with your own
+    private int x = 3;
+    private int y = 4;
+    private int xy = 11;
+    private int cos = 5;
+
+    /**
+     * Constructor for objects of class test
+     */
+    public test()
+    {
+        double a = (x+1 * 4) * (2+y);
+        double b = x + y/2 + xy;
+        double c = (4 + cos) / 2;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+
+  
+}
---
+++ problem set 3/Short Problem 3/Problem3.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Problem3 here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Problem3
+{
+    public void main() {
+        double hours = 2345.9;
+        int days = (int) (hours / 24.0);
+        System.out.println("days = " + days);
+    }
+}
